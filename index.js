cfa8ca0a3493108364494a5daa04160488f47f83
damian0s10 <damian.wasiluk10@gmail.com>
2020-12-06 18:35:05 +0100

    upload img

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const uploadBtn = document.querySelector(".upload-btn");
+const uploadInput = document.querySelector(".upload-input");
+const img = document.querySelector("img.image");
+
+const uploadImg = () => {
+  uploadInput.click();
+};
+
+function handleFile() {
+  const file = this.files[0];
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    img.src = reader.result;
+  };
+  reader.readAsDataURL(file);
+}
+
 // Register service worker to control making site work offline
 window.onload = () => {
     "use strict";
@@ -6,4 +24,6 @@ window.onload = () => {
                 .register('./sw.js')
                 .then(function() { console.log('Service Worker Registered'); });
     }
+    uploadBtn.addEventListener("click", uploadImg);
+    uploadInput.addEventListener("change", handleFile);
 };
